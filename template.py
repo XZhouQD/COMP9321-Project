b74c04bc2aeb70ad79c1bc817217fa3b24188af7
zhou394626288 <zhou394626288@gmail.com>
2019-11-11 22:44:28 +1100

    initial template python file for headers

---
+++ template.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+'''
+UNSW COMP9321 19T3
+Assignment 2
+Team Data Analysis Towards Abbreviation
+
+Members (In order of Last Name)
+Genyuan Liang z5235682
+Jiasi Lu z5122462
+Yefeng Niu z5149500
+Haiyan Xu z5135077
+Xiaowei Zhou z5108173
+
+Environment:
+Python 3.7
+'''
