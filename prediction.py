a45294914aca5557e2a91d4686573177d73b774e
Dolly <Dolly@MacBookdeMacBook-Air.local>
2019-11-17 19:34:10 +1100

    prediction preparation

--- prediction.py
+++ prediction.py
@@ -2,11 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-# input arg df: df return by clean(df)
-# input arg input_sub: suburb that customer inputing
-def get_listing_data(df,input_sub):
+
+def get_listing_data(df,listing):
     df = df.groupby(['listing_id'])
-    df = df.get_group(input_sub)
+    df = df.get_group(listing)
     df.reset_index(drop=True)
     df.index = range(len(df.index))
     return df
